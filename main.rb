a271e95c3eb00b0c2c73cbf2adb792d1dc303ed5
榎本拓真 <t3460515@gmail.com>
2020-12-07 01:27:47 +0900

    rubygame

--- main.rb
+++ main.rb
@@ -39,15 +39,53 @@ end
 
 class Monster
 
-	attr_reader :name, :offence, :defence
-	attr_accessor :hp
+	attr_reader :offence, :defence
+	attr_accessor :hp, :name
+
+	POWER_UP_RATE = 1.5
+	CALC_HALF_HP = 0.5
 
 	def initialize(**params)
 		@name = params[:name]
 		@hp = params[:hp]
 		@offence = params[:offence]
 		@defence = params[:defence]
+		@transform_flag = false
+		@trigger_of_transform = params[:hp] * CALC_HALF_HP
 	end
+
+	def attack(brave)
+		if @hp <= @trigger_of_transform && @transform_flag == false
+			@transform_flag = true
+			transform
+		end
+
+		puts "#{@name}の攻撃"
+
+		damage = @offence - brave.defence
+
+		brave.hp -= damage
+
+		puts "#{brave.name}は#{damage}のダメージを受けた"
+		puts "#{brave.name}の残りHPは#{brave.hp}だ"
+
+	end
+
+	private
+
+	def transform
+		transform_name = "ドラゴン"
+
+		puts <<~EOS
+		#{@name}は怒っている
+		#{@name}は#{transform_name}に変身した
+		EOS
+
+		@offence *= POWER_UP_RATE
+		@name = transform_name
+
+	end
+
 
 end
 
@@ -55,3 +93,4 @@ brave = Brave.new(name: "テリー", hp: 500, offence: 150, defence: 100)
 monster = Monster.new(name: "スライム", hp: 250, offence: 200, defence: 100)
 
 brave.attack(monster)
+monster.attack(brave)
